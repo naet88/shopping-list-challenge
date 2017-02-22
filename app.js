81dd80958c2167ae783f08269eefaec39a4733bf
naet88 <naet88@gmail.com>
2017-02-21 16:12:11 -0800

    finally sort of done

--- app.js
+++ app.js
@@ -2,82 +2,113 @@
 //.submit(), preventDefault(), toggleClass(), and closest().
 
 //Step 1: Single State Object
+$(function () {
 
 var state = {
-	items: [
-			{name: 'apples',
-			checked: false, 
-			},
-			{name: 'oranges',
-			checked: false,
-			},
-			{name: 'milk',
-			checked: true,
-			},
-			{name: 'bread',
-			checked: false,
-			},
-		]
-	}
+  items: [
+    {
+      name: 'apples',
+      checked: true,
+    },
+    {
+      name: 'oranges',
+      checked: false,
+    },
+    {
+      name: 'milk',
+      checked: true,
+    },
+    {
+      name: 'cucumbers',
+      checked: true,
+    },
+    {
+      name: 'bread',
+      checked: false,
+    },
+  ]
+};
 
 //Step 2: State modification functions (no jquery!)
 
 //adding items function
 
-var addItem = function(state, item) {
-    state.items.push(item);
+function addItem(state, item) {
+  state.items.push({
+    name: item,
+    checked: false,
+  });
 };
+
+//ORIGINAL
+// var addItem = function(state, item) {
+//   state.items.push({
+//     name: item,
+//     checked: false
+//   });
+// };
 
 //checked item function
-var checkedItem = function(state, item, newItemState) {
-	 state.items[itemIndex] = newItemState;
-};
+// function checkedItem(state, item, newItemState) {
+//   state.items[itemIndex] = newItemState;
+// };
 
-//Step 3: Render functions 
+//Step 3: Render functions
 
-var listItemHtml = 
-'<li>' +
-	'<span class="shopping-item"></span>' +
-	'<div class="shopping-item-controls">' +
-	  '<button class="shopping-item-toggle">' +
-	    '<span class="button-label">check</span>' +
-	  '</button>' +
-	  '<button class="shopping-item-delete">' +
-	    '<span class="button-label">delete</span>' +
-	  '</button>' +
-	'</div>' +
-'</li>'
+var listItemHtml =
+  '<li>' +
+  '<span class="shopping-item"></span>' +
+  '<div class="shopping-item-controls">' +
+  '<button class="shopping-item-toggle">' +
+  '<span class="button-label">check</span>' +
+  '</button>' +
+  '<button class="shopping-item-delete">' +
+  '<span class="button-label">delete</span>' +
+  '</button>' +
+  '</div>' +
+  '</li>'
 
 
 //render the new item added to the list.
-//not sure what the function should take at the moment
-//(thinkful said it must take state and element)  
-var renderItem = function(state, element) {
-	//inject new item and new item state into the DOM. Where do I put "newItem" variable?
-	//The default state of new item is unchecked. 
-	$(listItemHTML).find('shopping-item').text(newItem)
+
+var renderList = function(state, element) {
+  $('.shopping-list').html(' '); 
+
+  state.items.forEach(function(item) {
+    var itemElement = $(listItemHtml);
+    itemElement.find('.shopping-item').text(item.name);
+    if (item.checked == true) {
+    	itemElement.find('.shopping-item').addClass('shopping-item__checked');
+    }
+		element.append(itemElement);
+  });
 };
 
-// New function: IF item get checked, then 
-// shopping-item__checked should be added. maybe toggleclass is useful. 
 
-// var renderCheckedState = function(state, element) {
-	
-// }
+//Step 4: jQuery event listeners!!
 
-
-//Step 4: jQuery event listeners!! 
-
-//new item addition 
+//add new items 
 $('form').on('submit', (function(event) {
-	event.preventDefault();
-//adding new new item. 
-	addItem(state, item);
-	renderList(state, $('.shopping-list'));
+  event.preventDefault();
+  var newItem = $('#js-new-item').val();
+  addItem(state, newItem);
+  renderList(state, $('.shopping-list'));
 }));
 
-//probably need to modify this for event delegation...
-// $('button.shopping-item-toggle').on('click', (function(event) {
+//change item state
+$('.js-shopping-list').on('click', 'button.shopping-item-toggle', function(event) {
+   //get rid of class, shopping-item__checked and change state to true 
+  $(this).parent().siblings().toggleClass('shopping-item__checked');
+});
 
-// }));
+//delete item 
+$('.js-shopping-list').on('click', 'button.shopping-item-delete', function(event) {
+   //get rid of the entire list item 
+  $(this).parent().parent().remove('li');
+});
 
+
+// Initialize the original list 
+renderList(state, $('.shopping-list'));
+
+});
